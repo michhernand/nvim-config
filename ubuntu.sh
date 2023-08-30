09a22d5564a3bc43835bbc99a51c64871c6dcaf5
Michael Hernandez <michael_p_hernandez@mckinsey.com>
2023-08-29 22:31:24 -0600

    added ubuntu script

---
+++ ubuntu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# NVIM
+snap install nvim
+
+# LuaRocks package manager
+apt install luarocks
+
+# LazyGit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+apt install lazygit /usr/local/bin
+
+# Packer
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
