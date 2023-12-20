d4ea441983c06859c0d7aa1fb307344e7a16a828
Michael Hernandez <michael_p_hernandez@mckinsey.com>
2023-12-20 16:58:08 -0600

    updated neocomposer keymappings

--- lua/hdz/plugins/neocomposer.lua
+++ lua/hdz/plugins/neocomposer.lua
@@ -1,5 +1,33 @@
 return {
   "ecthelionvi/NeoComposer.nvim",
   dependencies = { "kkharji/sqlite.lua" },
-  opts = {}
+  opts = {
+    notify = true,
+    delay_timer = 150,
+    queue_most_recent = false,
+    window = {
+      width = 60,
+      height = 10,
+      border = "rounded",
+      winhl = {
+        Normal = "ComposerNormal",
+      },
+    },
+    colors = {
+      bg = "#16161e",
+      fg = "#ff9e64",
+      red = "#ec5f67",
+      blue = "#5fb3b3",
+      green = "#99c794",
+    },
+    keymaps = {
+      play_macro = "Q",
+      yank_macro = "yq",
+      stop_macro = "cq",
+      toggle_record = "q",
+      cycle_next = "<M-n>",
+      cycle_prev = "<M-p>",
+      toggle_macro_menu = "<M-q>",
+    },
+  }
 }
