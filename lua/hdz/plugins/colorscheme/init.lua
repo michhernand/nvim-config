565781affee78ac3cfeb3a567d7217b339e75b3f
Michael Hernandez <mhernan88@gmail.com>
2024-01-26 19:46:10 +0000

    resolved syntax issue on server

--- lua/hdz/plugins/colorscheme/init.lua
+++ lua/hdz/plugins/colorscheme/init.lua
@@ -1,8 +1,10 @@
-return {
-    {
+if os.getenv("NEOVIM_ENV") == "server" then
+    return {}
+else
+    return {
 	    "folke/tokyonight.nvim",
 	    lazy = false,
 	    priority = 1000,
 	    opts = {},
-    },
-}
+    }
+end
--- lua/hdz/plugins/ui/bufferline.lua
+++ lua/hdz/plugins/ui/bufferline.lua
@@ -1,9 +1,20 @@
-return {
-    'akinsho/bufferline.nvim',
-    version = "*",
-    dependencies = 'nvim-tree/nvim-web-devicons',
-    config = function()
-        vim.opt.termguicolors = true
-        require("bufferline").setup()
-    end
-}
+if os.getenv("NEOVIM_ENV") == "server" then
+    return {
+        'akinsho/bufferline.nvim',
+        version = "*",
+        dependencies = 'nvim-tree/nvim-web-devicons',
+        config = function()
+            require("bufferline").setup()
+        end
+    }
+else
+    return {
+        'akinsho/bufferline.nvim',
+        version = "*",
+        dependencies = 'nvim-tree/nvim-web-devicons',
+        config = function()
+            vim.opt.termguicolors = true
+            require("bufferline").setup()
+        end
+    }
+end
