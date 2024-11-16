1a05670611e0730b1b609abc93400ddd4f15ec09
michael <mhernan88@gmail.com>
2024-11-16 00:09:38 -0600

    remove more java stuff

--- init.lua
+++ init.lua
@@ -90,8 +90,3 @@ vim.opt.cursorline = true
 vim.lsp.set_log_level("debug")
 require('vim.lsp.log').set_format_func(vim.inspect)
 
-local config = {
-    cmd = {'/opt/homebrew/bin/jdtls'},
-    root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
-}
-
