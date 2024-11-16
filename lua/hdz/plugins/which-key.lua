da4f9a11777af90cb0cbbc3faaca84623b0d5aba
michael <mhernan88@gmail.com>
2024-11-16 00:09:31 -0600

    update mappings

--- lua/hdz/plugins/which-key.lua
+++ lua/hdz/plugins/which-key.lua
@@ -14,90 +14,56 @@ return {
             return
         end
         -- local wk = require("which-key")
-        wk.register({
-            ["<leader>t"] = {
-                name = "trouble",
-                t = "toggle",
-                r = "refresh",
-            },
-            ["<leader>g"] = {
-                name = "go",
-                c = "term close",
-                t = "test",
-            },
-            ["<leader>h"] = {
-                name = "harpoon",
-                m = "menu",
-                k = "add file",
-                n = "nav next",
-                p = "nav prev",
-            },
-            ["<leader>f"] = {
-                name = "telescope",
-                f = { "find files" },
-                g = { "live grep" },
-                b = { "find buffers" },
-                h = { "find help tags" },
-                s = { "in-file grep" },
-            },
-            ["<leader>S"] = "toggle spectre",
-            ["<leader>s"] = {
-                name = "spectre",
-                w = "open visual",
-                p = "open file search",
-            },
+        wk.add({
+            {"<leader>R", group="resize pane"},
+            {"<leader>Rh", desc="left 5"},
+            {"<leader>Rj", desc="down 5"},
+            {"<leader>Rk", desc="up 5"},
+            {"<leader>Rl", desc="right 5"},
 
-            ["<leader>z"] = {
-                name = "telekasten",
-                f = "find_notes",
-                g = "search_notes",
-                d = "goto_today",
-                z = "follow_link",
-                n = "new_note",
-                c = "show_calendar",
-                b = "show_backlinks",
-                I = "insert_img_link",
-            },
-            ["<leader>G"] = "lazygit",
-            ["<leader>u"] = {
-                "undo tree",
-                u = { "launch" },
-            },
-            ["<leader>t"] = {
-                "trouble",
-                t = { "toggle" },
-                r = { "refresh" },
-            },
-            ["<leader>d"] = {
-                "delete (keep register)"
-            },
-            ["<leader>y"] = {
-                "yank (clipboard)"
-            },
-            ["<leader>Y"] = {
-                "yank line (clipboard)"
-            },
-            ["<leader>r"] = {
-                name = "refactoring",
-                e = { "extract" },
-                f = { "extract to file" },
-                v = { "extract variable" },
-                i = { "inline variable" },
-                b = { "extract block" },
-                g = { "extract blcok to file" },
-            },
-            ["<leader>w"] = {
-                name = "split",
-                v = "vertical",
-                h = "horizontal",
-            },
-            ["<leader>B"] = { "black" },
-            ["<leader>b"] = {
-                name = "buffers",
-                n = "next",
-                p = "prev",
-                d = "delete",
-            },
+            {"<leader>t", group="trouble"},
+            {"<leader>tt", desc="toggle"},
+            {"<leader>tr", desc="refresh"},
+
+            {"<leader>h", group="harpoon"},
+            {"<leader>hm", desc="menu"},
+            {"<leader>hk", desc="add file"},
+            {"<leader>hn", desc="nav next"},
+            {"<leader>hp", desc="nav prev"},
+
+            {"<leader>f", group="telescope"},
+            {"<leader>ff", desc="find files"},
+            {"<leader>fg", desc="live grep"},
+            {"<leader>fb", desc="find buffers"},
+            {"<leader>fh", desc="find help tags"},
+            {"<leader>fs", desc="in-file grep"},
+
+            {"<leader>G", desc="lazygit"},
+
+            {"<leader>t", group="trouble"},
+            {"<leader>tt", desc="toggle"},
+            {"<leader>tr", desc="refresh"},
+
+            {"<leader>d", desc="delete (keep register)"},
+            {"<leader>y", desc="yank (system clipboard)"},
+            {"<leader>Y", desc="yank line (system clipboard)"},
+
+            {"<leader>r", group="refactoring"},
+            {"<leader>re", desc="extract"},
+            {"<leader>rf", desc="extract to file"},
+            {"<leader>rv", desc="extract variable"},
+            {"<leader>ri", desc="inline variable"},
+            {"<leader>rb", desc="extract block"},
+            {"<leader>rg", desc="extract block to file"},
+
+            {"<leader>w", group="split"},
+            {"<leader>wv", desc="vertical"},
+            {"<leader>wh", desc="horizontal"},
+
+            {"<leader>b", group="buffers"},
+            {"<leader>bn", desc="next"},
+            {"<leader>bp", desc="prev"},
+            {"<leader>bd", desc="delete"}
         })
     end,
 }
