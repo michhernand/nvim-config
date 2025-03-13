f62a24833e5f9182f69f1989ea0e45ffc0b840d3
michael <mhernan88@gmail.com>
2025-03-13 00:06:22 -0500

    fixed breaking changes

--- lua/hdz/plugins/ide/trouble.lua
+++ lua/hdz/plugins/ide/trouble.lua
@@ -1,10 +1,39 @@
 return {
-    "folke/trouble.nvim",
-    event = { "BufReadPost", "BufNewFile" },
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {},
-    keys = {
-        { "<leader>tt", "<cmd>TroubleToggle<CR>" },
-        { "<leader>tr", "<cmd>TroubleRefresh<CR>" },
-    }
+	"folke/trouble.nvim",
+	event = { "BufReadPost", "BufNewFile" },
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {},
+	cmd = "Trouble",
+	keys = {
+		{
+			"<leader>tt",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Diagnostics (Trouble)",
+		},
+		{
+			"<leader>tT",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+			desc = "Buffer Diagnostics (Trouble)",
+		},
+		{
+			"<leader>cs",
+			"<cmd>Trouble symbols toggle focus=false<cr>",
+			desc = "Symbols (Trouble)",
+		},
+		{
+			"<leader>cl",
+			"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+			desc = "LSP Definitions / references / ... (Trouble)",
+		},
+		{
+			"<leader>xL",
+			"<cmd>Trouble loclist toggle<cr>",
+			desc = "Location List (Trouble)",
+		},
+		{
+			"<leader>xQ",
+			"<cmd>Trouble qflist toggle<cr>",
+			desc = "Quickfix List (Trouble)",
+		},
+	},
 }
