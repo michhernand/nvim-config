a9d911331f9be5b67cccbb56584661c6e28413f9
michael <mhernan88@gmail.com>
2025-03-20 12:50:44 -0500

    made key bindings consistent

--- lua/hdz/plugins/ide/trouble.lua
+++ lua/hdz/plugins/ide/trouble.lua
@@ -16,22 +16,22 @@ return {
 			desc = "Buffer Diagnostics (Trouble)",
 		},
 		{
-			"<leader>cs",
+			"<leader>ts",
 			"<cmd>Trouble symbols toggle focus=false<cr>",
 			desc = "Symbols (Trouble)",
 		},
 		{
-			"<leader>cl",
+			"<leader>tl",
 			"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
 			desc = "LSP Definitions / references / ... (Trouble)",
 		},
 		{
-			"<leader>xL",
+			"<leader>tL",
 			"<cmd>Trouble loclist toggle<cr>",
 			desc = "Location List (Trouble)",
 		},
 		{
-			"<leader>xQ",
+			"<leader>tQ",
 			"<cmd>Trouble qflist toggle<cr>",
 			desc = "Quickfix List (Trouble)",
 		},
