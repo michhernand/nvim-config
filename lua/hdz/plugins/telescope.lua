bf962cc664f10fb965fbe3bb38918386c4f9ca1c
michael <mhernan88@gmail.com>
2025-05-10 15:16:57 -0500

    bump telescope

--- lua/hdz/plugins/telescope.lua
+++ lua/hdz/plugins/telescope.lua
@@ -1,5 +1,5 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.2',
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
     event = { "BufReadPost", "BufNewFile" },
     lazy = true,
     dependencies = {
